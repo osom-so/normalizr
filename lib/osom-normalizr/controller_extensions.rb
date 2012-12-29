16874d7cddfd91726cc9b0952b666926de2ee2ca
Rafael Soto <fael.soto@gmail.com>
2012-12-29 09:56:19 -0600

    Controlador: Extendemos métodos de mini_hash

---
+++ lib/osom-normalizr/controller_extensions.rb
@@ -0,0 +1,55 @@
+module Osom::Normalizr
+  module ControllerExtensions
+    def self.included(base)
+      base.send(:include, InstanceMethods)
+    end
+
+    module InstanceMethods
+      def mini_hash str
+        str = hash_normalize str
+        str = "#{str}~#{current_user.try(:id) || request.remote_ip}~#{Date.today.to_s}"
+        shortnum Digest::MD5.hexdigest(str.to_s)[0..7].to_i(16)
+      end
+
+      def check_minihash str, hash
+        str = hash_normalize str
+        hashes = []
+        [:today, :tomorrow].each do |m|
+          tmp = "#{str}~#{current_user.try(:id) || request.remote_ip}~#{Date.send(m).to_s}"
+          hashes << shortnum(Digest::MD5.hexdigest(tmp.to_s)[0..7].to_i(16))
+        end
+        hashes.include? hash
+      end
+
+      def hash_normalize obj
+        if obj.kind_of? Array
+          arr = []
+          obj.each do |o|
+            arr << hash_normalize(o)
+          end
+          arr.join '~'
+        elsif !obj.kind_of? String
+          "#{obj.class.name}-#{obj.id}"
+        else
+          obj
+        end
+      end
+
+      def shortnum n
+        codeset = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        base = codeset.size
+        converted = ""
+        while n > 0 do
+          converted = "#{codeset[(n % base)]}#{converted}"
+          n = (n/base).floor
+        end
+
+        converted[0...3]
+      end
+
+      def current_user
+        nil
+      end
+    end
+  end
+end
--- lib/osom-normalizr/railtie.rb
+++ lib/osom-normalizr/railtie.rb
@@ -1,9 +1,14 @@
 require 'osom-normalizr/form_builder'
+require 'osom-normalizr/controller_extensions'
 
 module Osom::Normalizr
   class Railtie < Rails::Railtie
     initializer "osom-normalizr.form_helper" do
       ActionView::Base.send :include, FormHelper
+    end
+
+    config.to_prepare do
+      ApplicationController.send(:include, Osom::Normalizr::ControllerExtensions)
     end
     
     config.before_initialize do
