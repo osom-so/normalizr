799cc91487957f72b7af9be6e04400d48a46a37d
Rafael Soto <fael.soto@gmail.com>
2012-12-29 00:29:47 -0600

    Config: Quitamos field_with_error

--- lib/osom-normalizr/railtie.rb
+++ lib/osom-normalizr/railtie.rb
@@ -5,5 +5,9 @@ module Osom::Normalizr
     initializer "osom-normalizr.form_helper" do
       ActionView::Base.send :include, FormHelper
     end
+    
+    config.before_initialize do
+      config.action_view.field_error_proc = Proc.new { |html_tag, instance| "#{html_tag}".html_safe }
+    end
   end
 end
